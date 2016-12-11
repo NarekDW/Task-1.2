d55f7480f7fae8439d1900b5a3788b90fd478a48
Narek <narek.27.02.93@mail.ru>
2016-12-11 21:14:32 +0300

    First commit

---
+++ solution/src/main/java/ru/training/intro/second/Main.java
@@ -0,0 +1,11 @@
+package ru.training.intro.second;
+
+/**
+ * Created by Narek on 11.12.2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        MathSequence sequence=new MathSequence(0.001);
+        sequence.checkSequence();
+    }
+}
---
+++ solution/src/main/java/ru/training/intro/second/MathSequence.java
@@ -0,0 +1,48 @@
+package ru.training.intro.second;
+
+import static java.lang.Math.pow;
+
+/**
+ * Created by Narek on 11.12.2016.
+ */
+class MathSequence {
+    /*Переменная epsilan - окрестность последовательности,
+    * где выполняется условие М: 1/(1+i)^2
+    * */
+    double epsilan;
+
+    protected MathSequence(double epsilan){
+        this.epsilan=epsilan;
+    }
+
+    /*Метод возвращающий индекс элемента
+    * при котором выполняется условие M*/
+    protected int checkSequence(){
+        int i=1;    // индекс при a
+        while(!M(i)){
+            ++i;
+        }
+        printResult(i, epsilan); // Вывод результата
+        return i;
+    }
+
+    /*Метод, последовательно проверяющий истинность
+    * условия i-го элемента, при котором a<epsilan */
+    private boolean M(int i){
+        boolean condition=false; // condition - состояние условия неравенста
+        double a=(1/pow((1+i), 2)); // a - значение i-ой последовательности
+        if (a < epsilan) condition = true;
+
+        print(i, a); // Вывод номера последовательности и его значения
+        return condition;
+    }
+
+    private static void print(int i, double a){
+        System.out.printf("i =%3d : a = %7.6f%n", i, a);
+    }
+
+    private static void printResult(int i, double epsilan){
+        System.out.println("\nПри i = "+i+" выполняется условие М: a<epsilan" +
+                "\nГде epsilan = "+epsilan);
+    }
+}
