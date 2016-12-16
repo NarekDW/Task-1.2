acd47d68cd592ed29f7316effedd3974dbe3afa1
Narek <narek.27.02.93@mail.ru>
2016-12-16 13:46:57 +0300

    Completed

--- solution/src/main/java/ru/training/intro/second/Main.java
+++ solution/src/main/java/ru/training/intro/second/Main.java
@@ -1,12 +1,22 @@
 package ru.training.intro.second;
 
+import java.util.Scanner;
+
 /**
  * Created by Narek on 11.12.2016.
  */
 public class Main {
+
+    private static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args){
         MathSequence sequence=new MathSequence();
-        sequence.setEpsilan(0.001);
+        sequence.setEpsilan(getEpsilan());
         sequence.checkSequence();
+    }
+
+    private static double getEpsilan(){
+        System.out.println("Задайте эпсилан:");
+        return scan.nextDouble();
     }
 }
